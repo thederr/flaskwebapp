eb877e47eebcedf1b03bb7f3fd2a5e22caa139bf
derrick matheny <mathenyderrick@gmail.com>
2021-08-24 13:28:06 -0400

    created databse

--- website/models.py
+++ website/models.py
@@ -10,9 +10,13 @@ class Note(db.Model):
     #set up relationship between notes and users
     #foreign key 
     user_id=db.Column(db.Integer, db.ForeignKey('user.id'))
+    #we must pass a valid id of an exiting user MANY TO ONE relationship
+
+
 
 class User(db.Model,UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email=db.Column(db.String(150), unique= True)
     password=db.Column(db.String(150))
     first_name = db.Column(db.String(150))
+    notes = db.relationship('Note')
