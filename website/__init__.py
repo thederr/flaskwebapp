6ddc23c9d6f7705ef8700e1f1415f8f7661f3465
derrick matheny <mathenyderrick@gmail.com>
2021-08-24 09:01:53 -0400

    project setup

---
+++ website/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+def create_app():
+    app=Flask(__name__)
+    app.config['SECRET_KEY'] =' dsfdsf fdsfsdf f'
+
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views)
+    app.register_blueprint(auth)
+
+    return app
