d93204484534ef62b69d6c2f8223d7484952c947
derrick matheny <mathenyderrick@gmail.com>
2021-08-24 12:48:30 -0400

    finalizing set up

--- website/models.py
+++ website/models.py
@@ -7,6 +7,9 @@ class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
+    #set up relationship between notes and users
+    #foreign key 
+    user_id=db.Column(db.Integer, db.ForeignKey('user.id'))
 
 class User(db.Model,UserMixin):
     id = db.Column(db.Integer, primary_key=True)
