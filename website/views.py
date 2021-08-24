f63b7ec71d200e1fbdc314799a052dd61dff67cf
derrick matheny <mathenyderrick@gmail.com>
2021-08-24 15:54:33 -0400

    for some reason I'm not able to login, made an error somewhere

--- website/views.py
+++ website/views.py
@@ -1,8 +1,11 @@
+from website.auth import login
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user
 
 views = Blueprint('views',__name__)
 
 @views.route('/')
+@login_required
 def home():
     return render_template("home.html")
 
