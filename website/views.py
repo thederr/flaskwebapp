c8484f85780fc5dcfbcbc1aceeec245d439c2e55
derrick matheny <mathenyderrick@gmail.com>
2021-08-24 21:41:18 -0400

    added ability to delete notes

--- website/views.py
+++ website/views.py
@@ -1,6 +1,10 @@
+from flask.json import jsonify
 from website.auth import login
-from flask import Blueprint, render_template, flash
+from flask import Blueprint, render_template, flash, request, jsonify
 from flask_login import login_required, current_user
+from .models import Note
+from . import db
+import json
 
 views = Blueprint('views',__name__)
 
@@ -12,7 +16,21 @@ def home():
         if len(note) <1:
             flash('note is too short!', category='error')
         else:
+            new_note= Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
             flash('note added!', category='success')
 
     return render_template("home.html", user=current_user)
 
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    note= json.loads(request.data)
+    noteId= note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+
+    return jsonify({})
